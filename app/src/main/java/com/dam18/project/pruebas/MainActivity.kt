d7d84cc3d5ccf47a2a1ea5bf52a1f5056cfbc4ae
ldizbarros <laudiba@hotmail.com>
2019-01-11 10:55:59 +0100

    Hay dos activities, una para vertical y otra para horizontal

---
+++ app/src/main/java/com/dam18/project/pruebas/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.dam18.project.pruebas
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Añadimos una activity nueva (activity_main -- New -- Layout Resource File)
+        // Al cambiar a horizontal sale un toast
+        if (txtHorizontal != null) {
+            toast("Horizontal")
+        }
+
+        // Hacemos que cada vez que cliquemos el boton cambiemos de activity
+        btnVertical.setOnClickListener {
+            //TO DO
+        }
+
+
+    }
+}
